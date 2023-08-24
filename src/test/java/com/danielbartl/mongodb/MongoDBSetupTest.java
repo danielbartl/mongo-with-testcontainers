c7664c9d90dc68fb3f1391a313201338521a5d6f
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2023-08-24 14:53:18 +0200

    Latest Maven wrapper included.

--- src/test/java/com/danielbartl/mongodb/MongoDBSetupTest.java
+++ src/test/java/com/danielbartl/mongodb/MongoDBSetupTest.java
@@ -1,14 +1,41 @@
 package com.danielbartl.mongodb;
 
+import com.mongodb.client.MongoClients;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
+import java.util.ArrayList;
 
-public class AppTest
-{
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MongoDBSetupTest {
+
 
     @Test
-    public void testApp()
-    {
-        // does nothing
+    @DisplayName("Verifies whether the setup of testcontainers with the latest mongodb docker image is working properly.")
+    void verifySetUp() {
+
+        try (final var mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:latest"))) {
+
+            mongoDBContainer.start();
+
+            try (final var client = MongoClients.create(mongoDBContainer.getConnectionString())) {
+
+                final ArrayList<String> list = client.listDatabaseNames().into(new ArrayList<>());
+
+                assertEquals(3, list.size());
+
+                assertTrue(list.contains("admin"));
+                assertTrue(list.contains("local"));
+                assertTrue(list.contains("config"));
+
+            }
+
+        }
+
     }
+
 }
