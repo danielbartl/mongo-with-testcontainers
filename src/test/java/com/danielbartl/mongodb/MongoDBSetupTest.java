ef9545c28fbf94565c7773f2d146eaee902db026
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2023-08-24 14:48:14 +0200

    Initial commit

---
+++ src/test/java/com/danielbartl/mongodb/MongoDBSetupTest.java
@@ -0,0 +1,14 @@
+package com.danielbartl.mongodb;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest
+{
+
+    @Test
+    public void testApp()
+    {
+        // does nothing
+    }
+}
